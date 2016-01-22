9e6c4bf6ec85b27e9242025e76191d1de92e4289
Peter Caulfield <peterjcaulfield@gmail.com>
2016-01-22 21:58:41 +0000

    scope config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
- config = grunt.util._.extend({},
+ var config = grunt.util._.extend({},
     require('load-grunt-config')(grunt, {
         configPath: './grunt/tasks/options,
         loadGruntTasks: false,
