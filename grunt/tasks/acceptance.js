cebbd3cab5e47608aac86752dff970c41cb8366c
Peter Caulfield <peterjcaulfield@gmail.com>
2016-03-26 11:56:33 +0000

    add mocha config and es6 support for le tests

--- grunt/tasks/acceptance.js
+++ grunt/tasks/acceptance.js
@@ -1,10 +1,5 @@
-module.exports = function(grunt) {
-  /**
-   *
-   *
-   */
-  grunt.registerTask('acceptance', 'run the acceptance', function() {
-    grunt.task.run(['exec:pm2_start', 'protractor:default', 'exec:pm2_stop']);
-  });
-
-};
+module.exports = (grunt) => {
+  grunt.registerTask('acceptance', 'run the acceptance', () => {
+    grunt.task.run(['exec:pm2_start', 'protractor:default', 'exec:pm2_stop'])
+  })
+}
--- grunt/tasks/unit.js
+++ grunt/tasks/unit.js
@@ -1,13 +1,12 @@
-module.exports = function(grunt) {
-  /**
-   * Task to run the unit tests located in /tests/unit
-   *
-   * @usage grunt unit          - runs equivalent of grunt jasmine:test
-   *        grunt unit:config   - attempts to run the corresponding config in the jasmine config object
-   *                              e.g if you want coverage run grunt unit:coverage 
-   */
-  grunt.registerTask('unit', 'run the unit tests', function(config) {
-    grunt.task.run('jasmine:' + (arguments.length ? config : 'test')); 
-  });
-
-};
+/**
+  * Task to run the unit tests located in /tests/unit
+  *
+  * @usage grunt unit          - runs equivalent of grunt jasmine:test
+  *        grunt unit:config   - attempts to run the corresponding config in the jasmine config object
+  *                              e.g if you want coverage run grunt unit:coverage 
+  */
+module.exports = (grunt) => {
+  grunt.registerTask('unit', 'run the unit tests', (config) => {
+    grunt.task.run('mochaTest:' + (arguments.length ? config : 'test'))
+  })
+}
