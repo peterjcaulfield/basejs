b597884013124e8f74982c661998f6d07e696908
Peter Caulfield <peterjcaulfield@gmail.com>
2016-03-26 12:26:04 +0000

    add base webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+}
